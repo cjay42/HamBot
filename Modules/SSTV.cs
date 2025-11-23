6d8ba587af4fc5f110f649e508afb15cc391d980
Hannes <mingebag1337@gmail.com>
2025-11-23 12:39:34 +0100

    Projektdateien hinzufügen.

---
+++ Modules/SSTV.cs
@@ -0,0 +1,123 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using MQTTnet;
+using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+
+namespace HamBot.Modules
+{
+	internal class SSTVPayload
+	{
+		public string mode { get; set; } = "";
+		public int freq { get; set; }
+		public string sstvMode { get; set; } = "";
+		public string file { get; set; } = "";
+		public int width { get; set; }
+		public int height { get; set; }
+		public int line { get; set; }
+	}
+	internal class SSTV
+	{
+		private DiscordSocketClient _discordClient;
+		private IConfiguration _configuration;
+		private List<SocketGuildChannel> _channels;
+
+		private IMqttClient? _mqttClient;
+
+		public SSTV(DiscordSocketClient discordClient, IConfiguration configuration)
+		{
+			_configuration = configuration;
+			_discordClient = discordClient;
+			_channels = new List<SocketGuildChannel>();
+
+			_discordClient.Ready += BeginListen;
+			Console.WriteLine("SSTV service created");
+		}
+
+		private async Task RecievedImage(string jsonPayload)
+		{
+			SSTVPayload? sstvPayload = JsonSerializer.Deserialize<SSTVPayload>(jsonPayload);
+			if (sstvPayload is not null)
+			{
+				//download file from owrx server
+				HttpClient httpClient = new HttpClient();
+				var imageBytes = await httpClient.GetByteArrayAsync($"{_configuration["owrx"]}/files/{sstvPayload.file}");
+
+				using (var stream = new MemoryStream(imageBytes))
+				{
+					foreach (var channel in _channels)
+					{
+						if (channel is SocketTextChannel textChannel)
+						{
+							var embed = new EmbedBuilder()
+								.WithImageUrl($"attachment://{sstvPayload.file}")
+								.WithDescription($"**Mode**: {sstvPayload.sstvMode}\n**Frequency**: {sstvPayload.freq / 1000000.0:F3} MHz\n**Dimensions**: {sstvPayload.width}x{sstvPayload.height}")
+								.WithColor(Color.Blue)
+								.WithTimestamp(DateTimeOffset.Now)
+								.Build();
+							await textChannel.SendFileAsync(stream, sstvPayload.file, embed: embed);
+						}
+
+					}
+				}
+			}
+		}
+
+		private async Task StartMQTT()
+		{
+			var mqttFactory = new MqttClientFactory();
+
+			_mqttClient = mqttFactory.CreateMqttClient();
+			var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer(_configuration["mqttBroker"]).Build();
+
+			_mqttClient.ApplicationMessageReceivedAsync += async e =>
+			{
+				var message = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+				//Console.WriteLine($"Received application message: {message}");
+				if (message.Contains("file"))
+				{
+					await RecievedImage(message);
+				}
+				//return Task.CompletedTask;
+			};
+
+			await _mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+
+			var topicFilter = mqttFactory.CreateTopicFilterBuilder().WithTopic(_configuration["sstvTopic"]).WithAtLeastOnceQoS();
+
+			var mqttSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder().WithTopicFilter(topicFilter).Build();
+
+			var response = await _mqttClient.SubscribeAsync(mqttSubscribeOptions, CancellationToken.None);
+
+			Console.WriteLine("MQTT client subscribed to topic.");
+
+		}
+
+		public async Task BeginListen()
+		{
+			Console.WriteLine($"getting all channels...");
+			foreach (var guild in _discordClient.Guilds)
+			{
+				Console.WriteLine($"guild {guild.Id}");
+				foreach (var channel in guild.Channels)
+				{
+					if (channel.ChannelType == Discord.ChannelType.Text && channel.Name == "sstv")
+					{
+						_channels.Add(channel);
+						Console.WriteLine($"channel id {channel.Id}: {channel.Name}");
+					}
+				}
+			}
+
+			Console.WriteLine($"setting up MQTT client");
+
+			await StartMQTT();
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using Discord.WebSocket;
+using Discord;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HamBot
+{
+    internal class Program
+    {
+		private static IServiceProvider _services;
+
+		private static IConfiguration _configuration = new ConfigurationBuilder()
+				.AddEnvironmentVariables(prefix: "DC_")
+				.AddJsonFile("appsettings.json", optional: true)
+				.Build();
+
+		private static readonly DiscordSocketConfig _socketConfig = new()
+		{
+			GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.GuildMembers,
+			AlwaysDownloadUsers = true,
+		};
+		private static Task LogAsync(LogMessage msg)
+		{
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
+		}
+
+		public static async Task Main()
+		{
+			_services = new ServiceCollection()
+				.AddSingleton(_configuration)
+				.AddSingleton(_socketConfig)
+				.AddSingleton<DiscordSocketClient>()
+				.AddSingleton<Modules.SSTV>()
+				.BuildServiceProvider();
+
+			_services.GetRequiredService<Modules.SSTV>();
+
+			var client = _services.GetRequiredService<DiscordSocketClient>();
+
+			client.Log += LogAsync;
+
+			await client.LoginAsync(TokenType.Bot, _configuration["token"]);
+			await client.StartAsync();
+
+			await client.SetGameAsync($"Running on {System.Runtime.InteropServices.RuntimeInformation.OSDescription}", null, ActivityType.CustomStatus);
+
+			await Task.Delay(Timeout.Infinite);
+		}
+	}
+}
